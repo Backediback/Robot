e37def52b8aaf342d2e6c98bde81daddce6e4551
Your Name <you@example.com>
2024-09-20 12:50:35 +0200

    classes created

---
+++ RobotApp/ConsoleApp1/Classes/Robot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotApp.Classes
+{
+
+
+    public class Robot
+    {
+        private string _facing;
+        private int _positionX;
+        private int _positionY;
+        public string Facing { get => _facing; set => _facing = value; }
+        public int PositionX { get => _positionX; set => _positionX = value; }
+        public int PositionY { get => _positionY; set => _positionY = value; }
+
+        public Robot(int startXh, int startY, string facing)
+        {
+            _positionX = startXh;
+            _positionY = startY;
+            _facing = facing;
+        }
+    }
+}
